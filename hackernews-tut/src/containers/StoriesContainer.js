98ea37add3d9a2868a641efeaad9bdc32ef37901
Kate P <kaska129@op.pl>
2020-01-05 17:17:21 +0100

    add styles to containers

--- hackernews-tut/src/containers/StoriesContainer.js
+++ hackernews-tut/src/containers/StoriesContainer.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState } from 'react';
 import { getStoryIds } from "../services/hnApi";
 import { Story } from "../components/Story"
+import { GlobalStyle, StoriesContainerWrapper } from "../styles/StoryContainersStyles"
 
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds ] = useState([]);
@@ -10,9 +11,11 @@ export const StoriesContainer = () => {
     }, []);
     return (
     <>
+    <GlobalStyle/>
+    <StoriesContainerWrapper data-test-id="stories-container">
     <h1>Haker News Stories</h1>
       {storyIds.map(storyId =>( <Story key={storyId } storyId={storyId}/> ))};
-      </>
-
+      </StoriesContainerWrapper>
+</>
     )
 };
