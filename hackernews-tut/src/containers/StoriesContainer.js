cb369ce211bb977f609a35e68aa78a4eb778f01c
Kate P <kaska129@op.pl>
2020-01-05 13:25:31 +0100

    add storiesContainers

--- hackernews-tut/src/containers/StoriesContainer.js
+++ hackernews-tut/src/containers/StoriesContainer.js
@@ -1,14 +1,14 @@
 import React, {useEffect, useState } from 'react';
-import { getStoryIds, getStory } from "../services/hnApi";
+import { getStoryIds } from "../services/hnApi";
+import { Story } from "../components/Story"
 
 export const StoriesContainer = () => {
     const [storyIds, setStoryIds ] = useState([]);
 
     useEffect(() => {
         getStoryIds().then(data => setStoryIds(data));
-        getStory(21950475).then(data => console.log(data));
     }, []);
     return (
-        <p>{JSON.stringify(storyIds)}</p>
+       storyIds.map(storyId => <Story key={storyId } storyId={storyId}/> )
     )
 };
